fe75f87006e4a54ca7b7da8273db5215b59e42ad
runsf <mclausaudio@gmail.com>
2018-03-27 04:16:25 +0000

    Added mongoose and set up mongo db

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
 var express = require('express'),
     bodyparser = require('body-parser'),
-    app = express();
+    app = express(),
+    mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/gear_talk');
 app.use(bodyparser.urlencoded({extended: true}))
+app.set("view engine", "ejs");   
 
-app.set("view engine", "ejs");    
+var postSchema = new mongoose.Schema({
+    title: String,
+    image: String
+});
 
+var Post = mongoose.model("Post", postSchema);
 
-    var posts = [
-            {title:"tr909", image:"http://www.6am-group.com/wp-content/uploads/2015/09/Roland-909.jpg"},
-            {title:"tr909", image:"https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.6am-group.com%2Fwp-content%2Fuploads%2F2015%2F09%2FRoland-909.jpg&imgrefurl=http%3A%2F%2Fwww.6am-group.com%2F909day-tracks&docid=geJj70-fgHAs4M&tbnid=ORoFj8F2aC2UzM%3A&vet=10ahUKEwjqgdXV4_7ZAhUIFHwKHQqGD8IQMwh1KAIwAg..i&w=2400&h=1452&bih=699&biw=1212&q=909%20drum%20machine&ved=0ahUKEwjqgdXV4_7ZAhUIFHwKHQqGD8IQMwh1KAIwAg&iact=mrc&uact=8"},
-            {title:"tr909", image:"https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.6am-group.com%2Fwp-content%2Fuploads%2F2015%2F09%2FRoland-909.jpg&imgrefurl=http%3A%2F%2Fwww.6am-group.com%2F909day-tracks&docid=geJj70-fgHAs4M&tbnid=ORoFj8F2aC2UzM%3A&vet=10ahUKEwjqgdXV4_7ZAhUIFHwKHQqGD8IQMwh1KAIwAg..i&w=2400&h=1452&bih=699&biw=1212&q=909%20drum%20machine&ved=0ahUKEwjqgdXV4_7ZAhUIFHwKHQqGD8IQMwh1KAIwAg&iact=mrc&uact=8"}
-        ]
-
-
+// Post.create({
+//     title:"tr909-another one added", 
+//     image:"http://www.6am-group.com/wp-content/uploads/2015/09/Roland-909.jpg"
+// }, function (err, post){
+//     if (err){
+//         console.log(err)
+//     } else {
+//         console.log(post);
+//     }
     
+// })
+
 app.get("/", function(req, res){
     res.render("landing");
 })
 
 // this is called campgrounds in videos
 app.get("/posts", function(req, res){
-    
-    res.render("posts", {posts:posts});
+    //get all posts from db
+    Post.find({}, function(err, Posts){
+        if (err){
+            console.log(err);
+        } else {
+            res.render("posts", {posts:Posts});     
+        }
+           
+    })
+
 });
 
 
@@ -31,9 +50,14 @@ app.post("/posts", function(req, res){
     var title = req.body.title;
     var image = req.body.image;
     var newPost = {title: title, image: image};
-    posts.push(newPost);
-    //re direct to posts
-    res.redirect("/posts");
+    Post.create(newPost, function(err, newlyCreated){
+        if (err) {
+            console.log(err)
+        } else {
+            //re direct to posts
+            res.redirect("/posts");
+        }
+    })
 });
 
 app.get("/posts/new", function(req, res){
