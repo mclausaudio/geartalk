5b5809d92d4c31a36845d0b6d3289b33820c4ab9
runsf <mclausaudio@gmail.com>
2018-03-28 01:46:00 +0000

    Added more restful routes for individual posts

--- app.js
+++ app.js
@@ -9,47 +9,44 @@ app.set("view engine", "ejs");
 
 var postSchema = new mongoose.Schema({
     title: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Post = mongoose.model("Post", postSchema);
-
-// Post.create({
-//     title:"tr909-another one added", 
-//     image:"http://www.6am-group.com/wp-content/uploads/2015/09/Roland-909.jpg"
-// }, function (err, post){
-//     if (err){
-//         console.log(err)
-//     } else {
-//         console.log(post);
-//     }
-    
-// })
+//     Post.create({
+//         title:"tr909-another one added", 
+//         image:"http://www.6am-group.com/wp-content/uploads/2015/09/Roland-909.jpg",
+//         description:"This is a great machine.  Classic sounds."
+//     }, function (err, post){
+//         if (err){
+//             console.log(err)
+//         } else {
+//             console.log(post);
+//         }
+//     });
 
 app.get("/", function(req, res){
     res.render("landing");
 })
 
-// this is called campgrounds in videos
+//INDEX Route
 app.get("/posts", function(req, res){
     //get all posts from db
     Post.find({}, function(err, Posts){
         if (err){
             console.log(err);
         } else {
-            res.render("posts", {posts:Posts});     
+            res.render("index", {posts:Posts});     
         }
-           
     })
-
 });
-
-
+// CREATE PATH
 app.post("/posts", function(req, res){
-    //get data from form and push to array
     var title = req.body.title;
     var image = req.body.image;
-    var newPost = {title: title, image: image};
+    var description = req.body.description;
+    var newPost = {title: title, image: image, description: description};
     Post.create(newPost, function(err, newlyCreated){
         if (err) {
             console.log(err)
@@ -59,10 +56,29 @@ app.post("/posts", function(req, res){
         }
     })
 });
-
+//NEW Route
 app.get("/posts/new", function(req, res){
     res.render("new.ejs")
 })
+
+//Show template
+app.get("/posts/:id", function(req, res){
+    //get post from db
+    Post.findById(req.params.id, function(err, post){
+        if (err){
+            console.log(err)
+        } else {
+            //show template with specific campground
+            res.render("show", {post: post});
+        }
+    });
+});
+
+    
+    
+    
+    
+    
     
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("server started");
